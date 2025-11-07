c43652cc1558c9b2c6de1c7c2f3ed247ecba5035
Dongun614 <jdw215415@gmail.com>
2025-11-07 16:10:41 +0900

    Faet: 10871.py

--- 10871.py
+++ 10871.py
@@ -1,21 +1,19 @@
-# 1. N과 X를 입력받는다.
-# 2. N번만큼 수를 입력받아서 A라는 수열에 저장시킨다
-# 3. A를 돌면서 X보다 작은 값을 result에 저장한다.
-# 4. Result를 출력한다
+import sys
 
-N = int(input())
-X = int(input())
-A = []
+try:
+    N, X = map(int, sys.stdin.readline().split())
+except ValueError:
+    sys.exit()
+    
+try:
+    A = list(map(int, sys.stdin.readline().split()))
+except ValueError:
+    sys.exit()
+    
 result = []
 
-
-for i in range(N):
-    temp = int(input())
-    A.append(temp)
-    
-for i in A:
-    if i < X:
-        result.append(i)
-
-for i in result:
-    print(result)
+for a in A:
+    if a < X:
+        result.append(str(a))
+        
+print(" ".join(result))
